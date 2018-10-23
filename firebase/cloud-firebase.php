2c5c68d7c582a47a0a4953288c78064f0cd090eb
Thanapon Archint <thanapon.archint@gmail.com>
2018-10-23 16:38:10 +0700

    real-time DB connection

--- firebase/cloud-firebase.php
+++ firebase/cloud-firebase.php
@@ -1,9 +1,12 @@
 <?php
 
+
 require_once './vendor/autoload.php';
 
 use Kreait\Firebase\Factory;
 use Kreait\Firebase\ServiceAccount;
+use Kreait\Firebase;
+
 
 $serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/../firebase-key/petinlinever1-adaff558f03f.json');
 $firebase = (new Factory)
@@ -11,5 +14,3 @@ $firebase = (new Factory)
     ->create();
 
 $database = $firebase->getDatabase();
-
-die(print_r($database));
