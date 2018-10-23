9a7ca385b135251a3d49ab255861d949a964a34c
Thanapon Archint <thanapon.archint@gmail.com>
2018-10-23 14:32:28 +0700

    Add - composer and firebase SDK

---
+++ firebase/index.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once './vendor/autoload.php';
+
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
+
+$serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/../firebase-key/petinlinever1-adaff558f03f.json');
+$firebase = (new Factory)
+    ->withServiceAccount($serviceAccount)
+
+    ->create();
+
+$database = $firebase->getDatabase();
+
+die(print_r($database));
