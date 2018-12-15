1f1faea1e5b0d6fad9ac3a461cb8fc0273c5e57a
Thanapon <ponsmd1234@gmail.com>
2018-12-15 15:42:00 +0700

    new heroku cleardb configuration

--- dbconfig.php
+++ dbconfig.php
@@ -1,8 +1,18 @@
 <?php
-$servername = "localhost";
-$username = "pon";
-$password = "ilockpon1234";
-$dbname = "shd";
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+
+// $servername = "localhost";
+// $username = "pon";
+// $password = "ilockpon1234";
+// $dbname = "shd";
+$servername = $cleardb_server ;
+$username = $cleardb_username;
+$password = $cleardb_password;
+$dbname = $cleardb_db ;
 
 // Create connection
 $connect = new mysqli($servername, $username, $password, $dbname);
